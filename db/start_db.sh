564228732a52edd550dec095c15ce1edb8787652
Jesus Samitier <contacto@jasamitier.com>
2023-02-07 23:32:26 +0100

    fix user group to start the db

--- db/start_db.sh
+++ db/start_db.sh
@@ -1,1 +1,1 @@
-docker run --rm --name mariadb_dev -e MARIADB_ROOT_PASSWORD=mysql -d -p 3808:3808 -v $(pwd)/db/data:/var/lib/mysql:rw --user 1000:50 mariadb:latest --port 3808
+docker run --rm --name mariadb_dev -e MARIADB_ROOT_PASSWORD=mysql -d -p 3808:3808 -v $(pwd)/db/data:/var/lib/mysql:rw --user $(id -u):$(id -g) mariadb:latest --port 3808
